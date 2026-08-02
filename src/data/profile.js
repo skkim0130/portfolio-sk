dfe6299b7b10fe6218b3977d49f167feb6efd555
김선경 <mimcom43@gmail.com>
2026-08-03 01:22:38 +0900

    refactor: 프로필 소개 문구 변경

--- src/data/profile.js
+++ src/data/profile.js
@@ -9,7 +9,7 @@ export const PROFILE_DATA = {
   role: "Web Publisher",
   career: `${career.years}년 차`,
   intro:
-    "다양한 웹 프로젝트 경험을 바탕으로, 유지보수성과 확장성을 고려한 UI를 구현합니다.",
+    "구축부터 운영까지의 경험을 바탕으로, 유지보수성과 확장성을 고려한 UI를 구현합니다.",
   notionUrl:
     "https://smoggy-linen-a30.notion.site/2ee1a88239c380de94d1ed9d1f2bf461",
   githubUrl: "https://github.com/skkim0130/portfolio-sk",
